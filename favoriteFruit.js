2e274422537e1b5f3426179e2008130c6a86af61
Me <Cherie@My-MacBook-Pro-2.local>
2017-10-12 21:35:38 -0400

    first commit

---
+++ favoriteFruit.js
@@ -0,0 +1,9 @@
+var fruit= "mango"; 
+var myFruits = ["mangos", "apples", "peaches"]; 
+
+fill(199, 37, 37);
+text(myFruits[0], 10, 30); 
+text(myFruits[1], 10, 50); 
+text(myFruits[2], 10, 70); 
+
+text("My top " + myFruits.length + " favorite fruits", 10, 100);
